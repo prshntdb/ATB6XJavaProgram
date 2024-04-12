f7bae104280a8c2993e39d5c808bfd150429b49d
Prashant Dubey <prshntdb385@gmail.com>
2024-04-12 19:47:38 +0530

    Fast

---
+++ src/LAB001.java
@@ -0,0 +1,10 @@
+public class LAB001 {
+    public static void main(String[] args) {
+        System.out.println("Prashant");
+        System.out.println("Dubey");
+
+
+        System.out.println("Monu");
+        System.out.println("Dubey");
+    }
+}

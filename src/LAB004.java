7e17a2190075f004e06642be527e9ae2bf44a138
Prashant Dubey <prshntdb385@gmail.com>
2024-04-13 08:36:33 +0530

    Fast

---
+++ src/LAB004.java
@@ -0,0 +1,12 @@
+
+
+public class LAB004 {
+    public static void main(String[] args) {
+        //System.out.println("Variables");
+        int age=65;
+        age=66;
+        System.out.println(age);
+
+    }
+    }
+
